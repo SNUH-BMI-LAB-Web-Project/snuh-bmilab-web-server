d589a9c859141d03e95fcaba426cfcc68480410a
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2025-05-26 05:43:53 +0900

    feat: timeline 타입 추가

--- src/main/java/com/bmilab/backend/domain/project/enums/TimelineType.java
+++ src/main/java/com/bmilab/backend/domain/project/enums/TimelineType.java
@@ -9,7 +9,8 @@ public enum TimelineType {
     EXTERNAL_MEETING("외부 미팅"),
     INTERNAL_MEETING("내부 미팅"),
     SUBMISSION_DEADLINE("제출 마감"),
-    MATERIAL_SHARE("자료 공유");
+    MATERIAL_SHARE("자료 공유"),
+    ETC("기타");
 
     private final String description;
 }
