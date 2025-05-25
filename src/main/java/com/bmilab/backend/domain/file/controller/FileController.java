1d4ce5498611a81afbfef6f5bcec30c5d22c6b3a
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2025-05-25 20:36:26 +0900

    feat: FileController 클래스 생성

---
+++ src/main/java/com/bmilab/backend/domain/file/controller/FileController.java
@@ -0,0 +1,13 @@
+package com.bmilab.backend.domain.file.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/files")
+@RequiredArgsConstructor
+public class FileController {
+}
