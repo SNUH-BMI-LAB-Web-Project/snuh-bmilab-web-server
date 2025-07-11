be0d461c141f0524f878b3b802d014a60d2303b4
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2025-07-11 14:33:37 +0900

    fix: user.countDistinct() 오류 수정

--- src/main/java/com/bmilab/backend/domain/user/repository/UserRepositoryCustomImpl.java
+++ src/main/java/com/bmilab/backend/domain/user/repository/UserRepositoryCustomImpl.java
@@ -137,7 +137,7 @@ public class UserRepositoryCustomImpl implements UserRepositoryCustom {
         List<UserInfoQueryResult> results = new ArrayList<>(resultMap.values());
 
         Long total = queryFactory
-                .select(user.countDistinct())
+                .select(user.id.countDistinct())
                 .leftJoin(userInfo).on(userInfo.user.eq(user))
                 .leftJoin(userProjectCategory).on(userProjectCategory.user.eq(user))
                 .leftJoin(category).on(userProjectCategory.category.eq(category))
