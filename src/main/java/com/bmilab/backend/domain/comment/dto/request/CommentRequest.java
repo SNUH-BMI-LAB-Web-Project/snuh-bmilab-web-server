715508a97edae9c1116b26bc007618c770694f16
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2025-07-25 16:33:49 +0900

    fix: 댓글 요청 검증 수정

--- src/main/java/com/bmilab/backend/domain/comment/dto/request/CommentRequest.java
+++ src/main/java/com/bmilab/backend/domain/comment/dto/request/CommentRequest.java
@@ -3,6 +3,7 @@ package com.bmilab.backend.domain.comment.dto.request;
 import com.bmilab.backend.domain.comment.enums.CommentDomainType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record CommentRequest(
         @Schema(description = "댓글 내용", example = "댓글 메시지 예시입니다.")
@@ -10,11 +11,11 @@ public record CommentRequest(
         String message,
 
         @Schema(description = "댓글 도메인 타입")
-        @NotBlank(message = "댓글 도메인 타입은 필수입니다.")
+        @NotNull(message = "댓글 도메인 타입은 필수입니다.")
         CommentDomainType domainType,
 
-        @Schema(description = "엔티티 ID", example = "1")
-        @NotBlank(message = "엔티티 ID는 필수입니다.")
+        @Schema(description = "댓글 엔티티 ID", example = "1")
+        @NotNull(message = "엔티티 ID는 필수입니다.")
         Long entityId
 ) {
 }
