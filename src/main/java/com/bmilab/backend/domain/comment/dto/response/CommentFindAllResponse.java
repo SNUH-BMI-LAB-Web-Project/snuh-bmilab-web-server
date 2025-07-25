c9550b0327e6085d2e6b7f1cdb738bf08af20b16
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2025-07-25 15:48:22 +0900

    feat: 댓글 응답에 사용자 정보 추가

--- src/main/java/com/bmilab/backend/domain/comment/dto/response/CommentFindAllResponse.java
+++ src/main/java/com/bmilab/backend/domain/comment/dto/response/CommentFindAllResponse.java
@@ -2,6 +2,7 @@ package com.bmilab.backend.domain.comment.dto.response;
 
 import com.bmilab.backend.domain.comment.entity.Comment;
 import com.bmilab.backend.domain.comment.enums.CommentDomainType;
+import com.bmilab.backend.domain.user.dto.response.UserSummary;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
@@ -21,6 +22,7 @@ public record CommentFindAllResponse(
     @Builder
     public record CommentSummary(
             Long commentId,
+            UserSummary user,
             String message,
             CommentDomainType domainType,
             Long entityId,
@@ -32,6 +34,7 @@ public record CommentFindAllResponse(
 
             return CommentSummary.builder()
                     .commentId(comment.getId())
+                    .user(UserSummary.from(comment.getUser()))
                     .message(comment.getMessage())
                     .domainType(comment.getDomainType())
                     .entityId(comment.getEntityId())
