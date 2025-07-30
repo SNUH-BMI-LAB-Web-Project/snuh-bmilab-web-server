48179fd9c987f7d2578868afa63841b725714261
wngktjd13 <seonguk3553@kookmin.ac.kr>
2025-07-30 18:05:00 +0900

    refactor: ‘프로젝트’ 용어를 ‘연구’로 변경하여 용어 통일

--- src/main/java/com/bmilab/backend/domain/project/controller/AdminProjectApi.java
+++ src/main/java/com/bmilab/backend/domain/project/controller/AdminProjectApi.java
@@ -4,7 +4,6 @@ import com.bmilab.backend.domain.project.dto.request.ExternalProfessorRequest;
 import com.bmilab.backend.domain.project.dto.request.ProjectPinRequest;
 import com.bmilab.backend.domain.project.dto.response.ExternalProfessorFindAllResponse;
 import com.bmilab.backend.global.exception.ErrorResponse;
-import com.bmilab.backend.global.security.UserAuthInfo;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -13,11 +12,10 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@Tag(name = "(Admin) Project", description = "(관리자용) 프로젝트 관리 API")
+@Tag(name = "(Admin) Project", description = "(관리자용) 연구 관리 API")
 public interface AdminProjectApi {
 
     @Operation(summary = "외부 교수 등록", description = "외부 교수 정보를 등록하는 POST API")
@@ -89,7 +87,6 @@ public interface AdminProjectApi {
             }
     )
     ResponseEntity<Void> updateProjectPinStatus(
-            @AuthenticationPrincipal UserAuthInfo userAuthInfo,
             @PathVariable Long projectId,
             @RequestBody @Valid ProjectPinRequest request
     );
