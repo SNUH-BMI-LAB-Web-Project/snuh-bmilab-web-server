72ffe3743ff0127217b2b1a1d200556510cbf124
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2025-08-12 13:08:44 +0900

    chore: 업무보고 batch 배포 환경에서만 동작하도록 수정

--- src/main/java/com/bmilab/backend/domain/report/service/ReportSchedulerService.java
+++ src/main/java/com/bmilab/backend/domain/report/service/ReportSchedulerService.java
@@ -3,6 +3,7 @@ package com.bmilab.backend.domain.report.service;
 import com.bmilab.backend.global.email.EmailSender;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +14,7 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 @Service
+@Profile("prod")
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ReportSchedulerService {
