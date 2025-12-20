ce0f8f997c1c907fbff7785d15ae192ec6480513
wngktjd13 <seonguk3553@kookmin.ac.kr>
2025-12-21 00:28:26 +0900

    feat: 공통 리소스 NOT_FOUND 에러 코드 추가

--- src/main/java/com/bmilab/backend/global/exception/GlobalErrorCode.java
+++ src/main/java/com/bmilab/backend/global/exception/GlobalErrorCode.java
@@ -10,6 +10,7 @@ public enum GlobalErrorCode implements ErrorCode {
     INTERNAL_SERVER_ERROR("응답 처리 중, 예외가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
     SECURITY_USER_NOT_FOUND("로그인된 유저의 정보를 불러오는 데 실패했습니다.", HttpStatus.BAD_REQUEST),
     INVALID_ACCESS_TOKEN("유효하지 않은 토큰이거나, 토큰의 유효기한이 만료되었습니다.", HttpStatus.UNAUTHORIZED),
+    GLOBAL_NOT_FOUND("해당 리소스를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
     FILE_UPLOAD_FAILED("파일 업로드에 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
     FILE_NOT_FOUND("파일을 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
 
